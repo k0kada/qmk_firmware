6c36548dcfa0516cabfdb64c81730f0f07dd6efe
Koh Okada <koh.okada@gmail.com>
2019-08-25 07:12:46 +0900

    add media key

--- keyboards/crkbd/keymaps/default/keymap.c
+++ keyboards/crkbd/keymaps/default/keymap.c
@@ -128,7 +128,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------.                ,-----------------------------------------.
       RESET,RGBRST, KC_XX, KC_XX, KC_XX, KC_XX,                  KC_XX, KC_XX, KC_XX, KC_XX, KC_XX, KC_XX,\
   //|------+------+------+------+------+------|                |------+------+------+------+------+------|
-    RGB_TOG,RGB_HUI,RGB_SAI,RGB_VAI,KC_XX,KC_XX,                 KC_XX, KC_XX, KC_XX, KC_XX, KC_XX, KC_XX,\
+    RGB_TOG,RGB_HUI,RGB_SAI,RGB_VAI,KC_XX,KC_XX,               KC_MUTE,KC_VOLD,KC_VOLU,KC_MPLY,KC_MNXT,KC_MPRV,\
   //|------+------+------+------+------+------|                |------+------+------+------+------+------|
     RGB_MOD,RGB_HUD,RGB_SAD,RGB_VAD,KC_XX,KC_XX,                 KC_XX, KC_XX, KC_XX, KC_XX, KC_XX, KC_XX,\
   //|------+------+------+------+------+------+------|  |------+------+------+------+------+------+------|
--- keyboards/crkbd/keymaps/default/rules.mk
+++ keyboards/crkbd/keymaps/default/rules.mk
@@ -13,4 +13,7 @@ SRC +=  ./lib/glcdfont.c \
 #   change to "no" to disable the options, or define them in the Makefile in
 #   the appropriate keymap folder that will get included automatically
 #
-MOUSEKEY_ENABLE = yes        # Mouse keys(+4700)
+MOUSEKEY_ENABLE = yes       # Mouse keys(+4700)
+EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
+# feature RGBMATRIX
+RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight.
